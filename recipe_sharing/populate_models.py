b93e45a0fdb7b45224052b1f6407d47302f390f3
Franckevicius <49790557+Franckevicius@users.noreply.github.com>
2022-03-11 09:46:41 +0200

    Add recipe population

--- recipe_sharing/populate_models.py
+++ recipe_sharing/populate_models.py
@@ -28,12 +28,12 @@ def create_user(username:str, email:str, password:str, about_me:str):
     return u
 
 
-def create_recipe(title:str, ingredients:List[Ingredient], description:str, 
+def create_recipe(author:UserProfile, title:str, ingredients:List[Ingredient], description:str, 
                   estimated_nutrition:int, estimated_price:int, minutes_to_prepare:int, picture:str):
-    r = Recipe.objects.get_or_create(title=title, description=description, estimated_nutrition=estimated_nutrition, 
-                                      estimated_price=estimated_price, minutes_to_prepare=minutes_to_prepare)[0]
+    r = Recipe.objects.get_or_create(author=author, title=title, description=description, estimated_nutrition=estimated_nutrition, 
+                                     estimated_price=estimated_price, minutes_to_prepare=minutes_to_prepare)[0]    
     r.picture = picture
-    r.ingredients = ingredients
+    r.ingredients.set(ingredients)
     r.save()
     return r
 
@@ -41,47 +41,46 @@ def create_recipe(title:str, ingredients:List[Ingredient], description:str,
 def populate():
     ingredient_objects, user_objects, recipe_objects = [], [], []
     
-    ingredients = [
+    ingredient_data = [
         {"name":"Water", "quantity":100, "units":"ml" },
         {"name":"Tomato", "quantity":200, "units":"g" },
         {"name":"Potato", "quantity":500, "units":"g" },
         {"name":"Salt", "quantity":5, "units":"g" },
     ]
 
-    users = [
+    user_data = [
         {"username":"User1", "email":"User1@email.com", "password":"user1password", "about_me":"User1_about_me"},
         {"username":"User2", "email":"User2@email.com", "password":"user2password", "about_me":"User2_about_me"},
         {"username":"User3", "email":"User3@email.com", "password":"user3password", "about_me":"User3_about_me"},
         {"username":"User4", "email":"User4@email.com", "password":"user4password", "about_me":"User4_about_me"}
     ]
 
-    for ingredient in ingredients:
+    for ingredient in ingredient_data:
         ingredient_objects.append(create_ingredient(**ingredient))
 
     for ingredient in Ingredient.objects.all():
         print(ingredient)
 
 
-    for user in users:
+    for user in user_data:
         user_objects.append(create_user(**user))
 
     for user in UserProfile.objects.all():
         print(user)
 
-    recipes = [
-        {"title":"Recipe1", "ingredients":[ingredients[0], ingredients[1]], "description":"desc1", "estimated_nutrition":1, 
-         "estimated_price":1, "minutes_to_prepare":15, "picture":"pictures_placeholder/1.png"},
-        {"title":"Recipe1", "ingredients":[ingredients[1], ingredients[2]], "description":"desc2", "estimated_nutrition":2, 
-         "estimated_price":2, "minutes_to_prepare":25, "picture":"pictures_placeholder/2.png"},
-        {"title":"Recipe1", "ingredients":[ingredients[2], ingredients[3]], "description":"desc3", "estimated_nutrition":3, 
-         "estimated_price":3, "minutes_to_prepare":35, "picture":"pictures_placeholder/3.png"},
-        {"title":"Recipe1", "ingredients":[ingredients[3], ingredients[2]], "description":"desc4", "estimated_nutrition":2, 
-         "estimated_price":2, "minutes_to_prepare":45, "picture":"pictures_placeholder/4.png"},
-         
+    recipe_data = [
+        {"author":user_objects[0], "title":"Recipe1", "ingredients":[ingredient_objects[0], ingredient_objects[1]], "description":"desc1", 
+         "estimated_nutrition":1, "estimated_price":1, "minutes_to_prepare":15, "picture":"pictures_placeholder/1.png"},
+        {"author":user_objects[1], "title":"Recipe2", "ingredients":[ingredient_objects[1], ingredient_objects[2]], "description":"desc2", 
+         "estimated_nutrition":2, "estimated_price":2, "minutes_to_prepare":25, "picture":"pictures_placeholder/2.png"},
+        {"author":user_objects[2], "title":"Recipe3", "ingredients":[ingredient_objects[2], ingredient_objects[3]], "description":"desc3", 
+        "estimated_nutrition":3, "estimated_price":3, "minutes_to_prepare":35, "picture":"pictures_placeholder/3.png"},
+        {"author":user_objects[3], "title":"Recipe4", "ingredients":[ingredient_objects[3], ingredient_objects[2]], "description":"desc4", 
+         "estimated_nutrition":2, "estimated_price":2, "minutes_to_prepare":45, "picture":"pictures_placeholder/4.png"},        
     ]
 
-    for recipe in recipes:
-        recipes.append(create_recipe(**recipe))
+    for recipe in recipe_data:
+        recipe_objects.append(create_recipe(**recipe))
 
     for recipe in Recipe.objects.all():
         print(recipe)
