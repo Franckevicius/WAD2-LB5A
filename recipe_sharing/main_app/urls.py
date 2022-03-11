a6490e2c5621e6b16153c829b3639dfaedb57563
2566493H <2566493h@student.gla.ac.uk>
2022-03-11 10:16:12 +0000

    Basic start to urls and views

---
+++ recipe_sharing/main_app/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from main_app import views
+
+app_name = 'main_app'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('register', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('my-profile/', views.user_profile, name='my-profile'),
+    path('my-profile/saved/', views.profile_saved, name='profile_saved')
+    path('search/', views.search, name='search'),
+    path('submit/', views.submit, name='submit'),
+]
--- recipe_sharing/main_app/views.py
+++ recipe_sharing/main_app/views.py
@@ -1,3 +1,82 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.urls import reverse
+from main_app.forms import UserForm, UserProfileForm
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from datetime import datetime
 
-# Create your views here.
+def index(request):
+    context_dict = {}
+    context_dict['boldmessage'] = 'This is the index page'
+
+    response = render(request, 'main_app/index.html', context=context_dict)
+    return response
+
+def about(request):
+    context_dict = {}
+    context_dict['boldmessage'] = 'This is the about page'
+
+    response = render(request, 'main_app/about.html', context=context_dict)
+    return response
+
+def register(request):
+
+    registered = False
+
+    if request.method =='POST':
+        user_form = UserForm(request.POST)
+        profile_form = UserProfileForm(request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['pictures']
+
+            profile.save()
+
+            registered = True
+        else:
+            print(user_form.errors, profile_form.errors)
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+
+    return render(request,
+                  'main_app/register.html',
+                  context = {'user_form': user_form,
+                             'profile_form': profile_form,
+                             'registered': registered})
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return redirect(reverse('main_app:index'))
+            else:
+                return HttpResponse("Your Recipe Sharing account is disabled.")
+        else:
+            print(f"Invalid login details: {username}, {password}")
+            return HttpResponse("Invalid login details supplied.")
+    else:
+        return render(request, 'main_app/login.html')
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect(reverse('rango:index'))
