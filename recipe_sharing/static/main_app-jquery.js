d0ec72741bad4ce0c934fdb81e2feeab9c791a52
Franckevicius <49790557+Franckevicius@users.noreply.github.com>
2022-03-25 13:41:15 +0200

    Complete AJAX search update client-side

--- recipe_sharing/static/main_app-jquery.js
+++ recipe_sharing/static/main_app-jquery.js
@@ -1,17 +1,15 @@
 $(document).ready(function() {
-    alert("Hello from main_app jQuery")
+    //alert("Hello from main_app jQuery")
 
-    //could not figure out how to fix this to work via AJAX
-    //currently search only work via URL, eg: "/search/?ingredient_input=p"
     $("#ingredient_input").keyup(function () {    
-        alert( $(this).val());
+        //alert( $(this).val());
 
         var query = $(this).val();
-        $.get("search/",
+        $.get("",
              {"ingredient_input": query}, 
              function(data) { 
-                //  $("#recipe_search_results").html(data);
-                alert(data);
+                $("#recipe_search_results").html($(data).filter("#recipe_search_results")); //.get("recipe_search_results")
+                console.log($(data).filter("#recipe_search_results"));
              })
     });
 
