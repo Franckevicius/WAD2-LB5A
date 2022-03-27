ca37a991745473cf61f8297a2b751b45ba1a90cd
Martin Suchan <2572779s@student.gla.ac.uk>
2022-03-27 00:10:04 +0000

    Further edits/tweaks to base and index, integrated recipe.html with Bootstrap, should be fully working and scalable to new recipes automatically

--- recipe_sharing/recipe_sharing/urls.py
+++ recipe_sharing/recipe_sharing/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),    
     path('', include('main_app.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # Trying to get the media to work (so recipes can use their placeholder pics)
